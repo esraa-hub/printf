3c6858232b62a8810f315c30a651c2e4f0111548
esraa-hub <engesraa3@gmail.com>
2023-10-16 05:33:04 -0700

    the main.h file

--- main.h
+++ main.h
@@ -8,6 +8,17 @@
 #include <unistd.h>
 
 #define NULL_STRING ("null")
+typedef struct function {
+    char *s;
+    int (*f)(va_list);
+} func;
+int printbinary_rec(unsigned int n);
+int printbinary(va_list cat);
+int printchar(va_list cat);
+int printstring(va_list cat);
+int printinteger(va_list cat);
 int _printf(const char *format, ...);
+int _putchar(char x);
+int _print_specifier(char *func, va_list cat);
 #endif
 
