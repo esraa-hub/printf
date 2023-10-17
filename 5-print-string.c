091f26ef8b74623be2c8caed338887b8fcdd43a2
esraa-hub <engesraa3@gmail.com>
2023-10-17 09:31:25 -0700

    this is 5 func

--- 5-print-string.c
+++ 5-print-string.c
@@ -1,9 +1,36 @@
 #include "main.h"
 /**
+ * nonchar-the function to print non char.
  *
+ * @cat: the list of input.
  *
- *
- *
- *
- * */
+ * Return: integer.
+ */
 
+int nonchar(va_list cat)
+{
+	char *nonchar;
+	int i, counter = 0;
+
+	nonchar = va_arg(cat, char*);
+	if (nonchar == NULL)
+	{
+		nonchar = "(null)";
+	}
+	for (i = 0; nonchar[i] != '\0'; i++)
+	{
+		if (nonchar[i] < 32 || nonchar[i] > 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			counter += 2;
+			if (nonchar[i] < 16)
+			{
+				_putchar('0');
+				counter++;
+			}
+			counter += print_uhex_num(nonchar[i]);
+		}
+	}
+	return (counter);
+}
